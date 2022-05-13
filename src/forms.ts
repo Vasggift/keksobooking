08d690169403dcf5f437e7448ef59f8334ae06ca
Vas <volk.vas.eli@gmail.com>
2022-05-13 15:10:50 +0300

    first task

--- src/forms.ts
+++ src/forms.ts
@@ -1,17 +1,21 @@
-export function disableForms(forms:HTMLFormElement[]){
-   forms.forEach(disableForm)
+export function disableForms(forms: HTMLFormElement[]) {
+  forms.forEach(disableForm)
 }
-function disableForm(form:HTMLFormElement){
-disableElements([...form.elements])
+function disableForm(form: HTMLFormElement) {
+  disableElements([...form.elements]);
+  setClassName(form)
 }
-function disableElements(elements:Element[]){
-elements.forEach(disableElement);
-} 
-interface FormElement{disabled:boolean}
-function isFormElement(element:object):element is FormElement{
-    return "disabled" in element
+function setClassName(form: Element) {
+  form.classList.add(`${form.className}--disabled`)
 }
-function disableElement(element:Element){
-  if (isFormElement(element) )  
-    element.disabled=true
+function disableElements(elements: Element[]) {
+  elements.forEach(disableElement);
+}
+interface FormElement { disabled: boolean }
+function isFormElement(element: object): element is FormElement {
+  return "disabled" in element
+}
+function disableElement(element: Element) {
+  if (isFormElement(element))
+    element.disabled = true
 }
